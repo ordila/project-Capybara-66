b78e8be4f1e2454cd690f7acdfa74f7b037cb9d9
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2024-02-09 12:09:51 +0200

    Bug fix in Selectors

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,9 +1,10 @@
 import { createSelector } from "@reduxjs/toolkit";
 
-export const selectExpenses = (state) => state.expenses;
-export const selectIncomes = (state) => state.incomes;
-export const selectFiltersCategory = (state) => state.filters.category;
-export const selectFiltersDate = (state) => state.filters.date;
+export const selectExpenses = (state) => state.transactions.expenses;
+export const selectIncomes = (state) => state.transactions.incomes;
+export const selectFiltersCategory = (state) =>
+  state.transactions.filters.category;
+export const selectFiltersDate = (state) => state.transactions.filters.date;
 
 export const selectFilteredExpenses = createSelector(
   [selectExpenses, selectFiltersCategory],
